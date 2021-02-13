c52a20cfe9a927ee9fc755a1761620f8c7f77f89
carcamo1 <carcamo1@users.noreply.github.com>
2021-02-12 20:33:54 -0500

    update

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,25 +1,25 @@
 const pics = document.querySelector(".images")
 
- function shift_left(){
-     const last_three = Array.from(pics.children).slice(4,7).reverse();
-     last_three.forEach((element) => {
+ function left(){
+     const last = Array.from(pics.children).slice(4,7).reverse();
+     last.forEach((element) => {
          pics.removeChild(element);
          pics.insertBefore(element, pics.children[0]);
      });
  }
- function shift_right(){
-    const first_three = Array.from(pics.children).slice(0,3);
-    first_three.forEach((element) => {
+ function right(){
+    const first = Array.from(pics.children).slice(0,3);
+    first.forEach((element) => {
         pics.removeChild(element);
         pics.appendChild(element);
     });
 }
-function load_page(){
+function home(){
     document.querySelector("button.arrow.prev").addEventListener("click", (event) => {
-        shift_left();
+        left();
     });
     document.querySelector("button.arrow.next").addEventListener("click", (event) => {
-        shift_right()
+        right()
     });
 }
-window.onload = load_page;
+window.onload = home;
