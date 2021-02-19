1bb0de74bc311260732de8f229d534cd9e53f1d3
carcamo1 <carcamo1@users.noreply.github.com>
2021-02-19 17:59:30 -0500

    update

---
+++ public/labs/lab_4/lab_4.spec.js
@@ -0,0 +1,141 @@
+/* eslint-disable no-unused-expressions */
+describe('Lab 4', () => {
+    it('Successfully loads with valid HTML', () => {
+      cy.fixture('test_values').then((json) => {
+        const lablink = `${json.test_context || ''}labs/lab_4/`;
+        cy.visit('https://shielded-woodland-08617.herokuapp.com/labs/lab_4/'); // change URL to match your dev URL
+        cy.htmlvalidate();
+      });
+    });
+  
+    it('Should contain your name and lab number within the page title', () => {
+      cy.fixture('test_values').then((json) => {
+        cy.title().then(($title) => {
+          const [name, lab, title] = [json.name, 'lab 4', $title].map((m) => m.toUpperCase());
+          expect(title.includes(name)).to.be.true;
+          expect(title.includes(lab)).to.be.true;
+        });
+      });
+    });
+  
+    it('should correctly link to a styles.css file', () => {
+      cy.get('head link[href="styles.css"]');
+    });
+  
+    it('Your lab should have a headline with your name and the lab number in it', () => {
+      cy.fixture('test_values').then((json) => {
+        cy.get('h1')
+          .then(($hh1) => {
+            const [name, lab, title] = [json.name, 'lab 4', $hh1.text()].map((m) => m.toUpperCase());
+            expect(title.includes(name)).to.be.true;
+            expect(title.includes(lab)).to.be.true;
+          });
+      });
+    });
+  
+    it('Should include an HTML form', () => {
+      cy.get('form');
+    });
+  
+    it('Should wrap form elements in an unordered list with a flex-outer class, for styling', () => {
+      cy.get('form ul.flex-outer');
+    });
+  
+    it('Should have inputs for first and last name, with ID of fname and lname', () => {
+      cy.get('form ul.flex-outer li input[type=text]')
+        .then(($ta) => {
+          const ID = $ta.attr('ID');
+          expect(ID).to.exist;
+        });
+  
+      cy.get('form ul.flex-outer li label[for="fname"]')
+        .contains('First Name');
+  
+      cy.get('form ul.flex-outer li label[for="lname"]')
+        .contains('Last Name');
+    });
+  
+    it('Should have inputs for email and phone', () => {
+      cy.get('form ul.flex-outer li input[type=tel]')
+        .then(($ta) => {
+          const ID = $ta.attr('ID');
+          expect(ID).to.exist;
+        });
+  
+      cy.get('form ul.flex-outer li input[type=email]')
+        .then(($ta) => {
+          const ID = $ta.attr('ID');
+          expect(ID).to.exist;
+        });
+    });
+  
+    it('Should wrap checkbox elements in a separate list for styling', () => {
+      cy.get('form ul.flex-outer li ul.flex-inner li input[type="checkbox"]');
+    });
+  
+    it('Should include a Textarea field that has 5 rows and 33 columns, labelled for messages', () => {
+      cy.get('form textarea[name="message"]')
+        .then(($ta) => {
+          const rows = Number($ta.attr('rows'));
+          const columns = Number($ta.attr('columns'));
+  
+          expect(rows).to.be.greaterThan(4);
+          expect(columns).to.be.greaterThan(32);
+        });
+    });
+  
+    it('All form controls should have a name attribute, for identification of data on server', () => {
+      cy.get('form textarea')
+        .then(($txt) => {
+          const name = $txt.attr('name');
+          expect(name).to.exist;
+        });
+      cy.get('form input')
+        .each(($txt) => {
+          const name = $txt.attr('name');
+          expect(name).to.exist;
+        });
+      cy.get('form button')
+        .then(($txt) => {
+          const name = $txt.attr('name');
+          expect(name).to.exist;
+        });
+    });
+  
+    it('All form controls should have appropriate labels and ID tags, for identification on the client', () => {
+      cy.get('form textarea')
+        .then(($txt) => {
+          const ID = $txt.attr('ID');
+          cy.get(`form label[for=${ID}]`);
+        });
+      cy.get('form input')
+        .each(($txt) => {
+          const ID = $txt.attr('ID');
+          cy.get(`form label[for=${ID}]`);
+        });
+      cy.get('form button')
+        .then(($txt) => {
+          const ID = $txt.attr('ID');
+          expect(ID).to.exist;
+        });
+    });
+  
+    // CSS TESTS START HERE
+  
+    it('Should use the submit button to POST material to the /api endpoint', () => {
+      cy.get('form').should('have.attr', 'method', 'post');
+      cy.get('button[type=submit]')
+        .click();
+      cy.contains('hello world', { matchCase: false });
+    });
+  
+    it('Should receive a string containing "Hello World" from the server - use res.send from Express docs for this', () => {
+      cy.fixture('test_values').then((json) => {
+        const lablink = `${json.test_context || ''}labs/lab_4/`;
+        cy.visit(lablink);
+        cy.get('button[type=submit]')
+          .click();
+        cy.contains('hello world', { matchCase: false });
+      });
+    });
+  });
